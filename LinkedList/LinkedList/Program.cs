4109a350c98820777e6099cf4505912420a9cc42
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-04 16:59:43 +0530

    [shabana] added UC1-CreateaLinkedList

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UseCase 1
+            CustomLinkedList obj = new CustomLinkedList();
+            //Insertion Operation
+            obj.InsertLast(56);
+            obj.InsertLast(30);
+            obj.InsertLast(70);
+            //Display Operation
+            Console.WriteLine("*****Insertion at Last*****");
+            obj.Display();
+        }
+    }
+}
